076cf5bc4d431b56c3685aedcc35d60b35515741
sepehr500 <sepehr411@gmail.com>
2020-08-20 13:35:00 -0400

    Remove conditional

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -7,11 +7,9 @@ export class AppGateway implements OnGatewayInit {
   constructor(private readonly airtableService: AirtableService) { }
   @WebSocketServer() wss: Server;
   afterInit(): void {
-    if (process.env.NODE_APP_INSTANCE === "0") {
-      setInterval(async () => {
-        const results = await this.airtableService.getStandings()
-        this.wss.emit("updateStandings", results)
-      }, 5000)
-    }
+    setInterval(async () => {
+      const results = await this.airtableService.getStandings()
+      this.wss.emit("updateStandings", results)
+    }, 5000)
   }
 }
